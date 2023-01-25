50f15bd5c1841cc3a09cbe1322d27a5b73c728e6
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-01-25 18:16:21 +0000

    [feature] - create start game page layout and style up

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import { styles } from './styles'
 
 const StartGame = () => {
   return(
     <View style={styles.container}>
-      <Text>Start the game</Text>
+      <Text style={styles.title}>Start the game</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Choose a number</Text>
+        <TextInput style={styles.input} placeholder='0'/>
+      </View>
+      <View style={styles.buttomContainer}>
+        <Button
+          style={styles.button} 
+          title='Reset'
+          onPress={() => null}
+          color='#9DAABE'
+        />
+        <Button
+          style={styles.button}
+          title='Confirm'
+          onPress={() => null}
+          color='#9DAABE'
+        />
+      </View>
     </View>
   );
 }
--- src/screens/start-game/styles.js
+++ src/screens/start-game/styles.js
@@ -3,6 +3,40 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 5,
+    paddingVertical: 10,
     alignItems: 'center'
+  },
+  title: {
+    fontSize: 18,
+    color: '#252525',
+    fontWeight: 'bold',
+    paddingVertical: 20
+  },
+  inputContainer: {
+    marginHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  label: {
+    paddingVertical: 20,
+    color: '#252525',
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  input: {
+    width: '100%',
+    minWidth: 80,
+    textAlign: 'center',
+    fontSize: 25,
+    borderBottomColor: "#9DAABE",
+    borderBottomWidth: 1,
+    paddingVertical: 10
+  },
+  buttomContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+    width: '75%',
+    justifyContent: 'space-around'
   }
 }); 
