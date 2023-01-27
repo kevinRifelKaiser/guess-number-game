474b872eec1b95c6a0efdb2605fcb6ed32685e24
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-01-27 01:11:00 +0000

    [feature] - validate input field

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,21 +1,40 @@
-import { View, Text, TextInput, Button } from 'react-native'
+import { View, Text, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useState } from 'react'
 import { styles } from './styles'
 import { Card } from '../../components'
 import { colors } from '../../constants'
 
 const StartGame = () => {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const onHandlerChange = (text) => {
+    setInputValue(text.replace(/[^0-9]/g, ''));
+  }
+
   return(
-    <View style={styles.container}>
-      <Text style={styles.title}>Start the game</Text>
-      <Card style={styles.inputContainer}>
-        <Text style={styles.label}>Choose a number</Text>
-        <TextInput keyboardType='numeric' style={styles.input} placeholder='0'/>
-        <View style={styles.buttonContainer}>
-          <Button title='Reset' onPress={() => null} color={colors.secondary} />
-          <Button title='Confirm' onPress={() => null} color={colors.secondary} />
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss()
+      }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Start the game</Text>
+        <Card style={styles.inputContainer}>
+          <Text style={styles.label}>Choose a number</Text>
+          <TextInput 
+            value={inputValue}
+            keyboardType='numeric' 
+            style={styles.input} 
+            placeholder='0' 
+            onChangeText={onHandlerChange} 
+          />
+          <View style={styles.buttonContainer}>
+            <Button title='Reset' onPress={() => null} color={colors.secondary} />
+            <Button title='Confirm' onPress={() => null} color={colors.secondary} />
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
