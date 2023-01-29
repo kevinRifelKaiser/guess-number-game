57cf330ae0e21a102533131c96b1017b3f9c8f29
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-01-29 21:31:53 +0000

    [feature] - create generate random number function in game screen

--- src/screens/game/index.jsx
+++ src/screens/game/index.jsx
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, Button } from 'react-native'
 import { styles } from './styles'
+import { Card, NumberContainer } from '../../components'
+import { colors } from '../../constants'
 
 const Game = ({ userNumber }) => {
+
+  const generateRandomNumber = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const randomNumber = Math.floor((Math.random() * (max - min)) + min);
+
+    if (randomNumber === exclude) {
+      return generateRandomNumber(min, max, exclude);
+    } else {
+      return randomNumber;
+    }
+  }
+
+  const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, userNumber));
+
   return(
     <View style={styles.container}>
-      <Text>Game</Text>
+      <Card style={styles.gameCard}>
+        <Text style={styles.gameTitle}>You have to find your number</Text>
+        <NumberContainer style={styles.numberContainer} number={currentGuess}/>
+        <View style={styles.buttonsContainer}>
+          <Button title='Decrease' onPress={() => null} color={colors.primary}/>
+          <Button title='Increase' onPress={() => null} color={colors.primary}/>
+        </View>
+        </Card>
     </View>
   );
 }
--- src/screens/game/styles.js
+++ src/screens/game/styles.js
@@ -1,8 +1,28 @@
 import { StyleSheet } from "react-native"
+import { colors } from '../../constants'
 
 export const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
-    alignItems: 'center'
+    alignItems: 'center',
+  },
+  gameCard: {
+    marginTop: 20,
+    height: 225,
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  gameTitle: {
+    fontSize: 18,
+    marginTop: 20,
+    color: colors.text,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    width: '100%',
+    justifyContent: 'space-between',
   }
 }); 
